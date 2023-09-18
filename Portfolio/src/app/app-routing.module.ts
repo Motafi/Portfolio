5fb9a541176d16c606026da0e0ff0f1f433e0e46
Desousa Romain <romain.desousa@etu.univ-orleans.fr>
2023-09-18 14:54:06 +0200

    Init styleScss

--- Portfolio/src/app/app-routing.module.ts
+++ Portfolio/src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header-component/header/header.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
