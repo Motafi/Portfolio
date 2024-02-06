eca7d502e2621e36d5114d7417f3c2e9c84ecf44
Desousa Romain <romain.desousa@etu.univ-orleans.fr>
2024-02-06 14:29:51 +0100

    Test pour merge

---
+++ Portfolio/src/app/test-merge/test-merge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-merge',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-merge.component.html',
+  styleUrl: './test-merge.component.scss'
+})
+export class TestMergeComponent {
+
+}
